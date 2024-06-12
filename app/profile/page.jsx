a7f4d5c428da6d290a91b5d8498866487810b5bf
Jack Vincent Pobre <jackvincentsanchez.pobre@bicol-u.edu.ph>
2024-06-12 21:54:55 +0800

    initial commit: home page done, fixed next auth, profile section WIP

---
+++ app/profile/page.jsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { useSession } from "next-auth/react"
+import Image from "next/image";
+import Link from "next/link";
+
+const Profile = () => {
+const { data: session } = useSession();
+
+  return (
+    <section>
+          <Image 
+          src={session?.user.image}
+          width={37}
+          height={37}
+          className="rounded-full"
+          
+          />
+    </section>
+  )
+}
+
+export default Profile
