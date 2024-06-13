39c72be4f921a11fe0b0214e332607c0ce23b135
Jack Vincent Pobre <jackvincentsanchez.pobre@bicol-u.edu.ph>
2024-06-13 23:07:14 +0800

    fixed GET API route and prompt card

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,11 +4,57 @@ import { useState, useEffect } from "react"
 
 import PromptCard from "./PromptCard"
 
-const Feed = () => {
+const PromptCardList = ({data, handleTagClick}) => {
   return (
-    <div>
-      Feed
+    <div className="mt-16 prompt_layout">
+        { data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        ))}
     </div>
+  )
+}
+
+
+const Feed = () => {
+
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+      const fetchPosts = async () => {
+          const response = await fetch("/api/prompt");
+          const data = await response.json();
+        console.log(data);
+           setPosts(data)
+      }
+      fetchPosts()
+  }, [])
+
+  const handleSearchChange = (e) => {
+
+  }
+
+  return (
+    <section className="feed">
+      <form action="" className="relative w-full flex-center">
+        <input 
+        type="text"
+        placeholder="Search for a tag or a username"
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className="w-full p-3 rounded-lg focus:outline-slate-300"
+        />
+      </form>
+      <PromptCardList
+      data={posts}
+      handleTagClick={ () => {} }
+      />
+
+    </section>
   )
 }
 
